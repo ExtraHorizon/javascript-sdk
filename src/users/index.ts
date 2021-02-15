d436711f88f8b130e11ad585c0df53d0807559e1
jonas <jonas.allard@fibricheck.com>
2021-02-15 09:51:31 +0100

    add exception responses

--- src/users/index.ts
+++ src/users/index.ts
@@ -9,9 +9,8 @@ const userServiceClient = new ApiClient('users', 'v1');
  * @permission Everyone can use this endpoint
  * @returns {boolean} success
  */
-
-export async function getHealth():Promise<boolean> {
-  const result:resultResponse = await userServiceClient.get('health');
+export async function getHealth(): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.get('health');
   return result.status === Results.Success;
 }
 /**
@@ -31,7 +30,7 @@ export async function getCurrent(): Promise<UserData> {
  * @params {string} rql Add filters to the requested list (optional)
  * @returns {UserDataList} UserDataList
  */
-export async function getList(rql = ''):Promise<UserDataList> {
+export async function getList(rql = ''): Promise<UserDataList> {
   return await userServiceClient.get(rql) as UserDataList;
 }
 
@@ -42,7 +41,7 @@ export async function getList(rql = ''):Promise<UserDataList> {
  * @params {string} rql Add filters to the requested list (optional)
  * @returns {UserDataList} UserDataList
  */
-export async function getPatients(rql = ''):Promise<UserDataList> {
+export async function getPatients(rql = ''): Promise<UserDataList> {
   return await userServiceClient.get(`patients/${rql}`) as UserDataList;
 }
 
@@ -53,7 +52,7 @@ export async function getPatients(rql = ''):Promise<UserDataList> {
  * @params {string} rql Add filters to the requested list (optional)
  * @returns {UserDataList} UserDataList
  */
-export async function getStaff(rql = ''):Promise<UserDataList> {
+export async function getStaff(rql = ''): Promise<UserDataList> {
   return await userServiceClient.get(`staff/${rql}`) as UserDataList;
 }
 
@@ -65,9 +64,15 @@ export async function getStaff(rql = ''):Promise<UserDataList> {
  * @permission VIEW_STAFF | scope:global | See a subset of fields for any user with a staff enlistment
  * @permission VIEW_USER | scope:global | See any user object
  * @params {string} userId of the targeted user (required)
+ * @throws 404: 
+ * {
+    "code": 16,
+    "name": "RESOURCE_UNKNOWN_EXCEPTION",
+    "message": "Requested resource is unknown"
+  }
  * @returns {UserData} UserData
  */
-export async function getById(userId:string): Promise<UserData> {
+export async function getById(userId: string): Promise<UserData> {
   return await userServiceClient.get(userId) as UserData;
 }
 
@@ -77,9 +82,15 @@ export async function getById(userId:string): Promise<UserData> {
  * @permission UPDATE_USER | scope:global | Update any user
  * @params {string} userId of the targeted user (required)
  * @params {any} data Fields to update
+ * @throws 404:
+ * {
+    "code": 16,
+    "name": "RESOURCE_UNKNOWN_EXCEPTION",
+    "message": "Requested resource is unknown"
+  }
  * @returns {UserData} UserData
  */
-export async function update(userId:string, data:any): Promise<UserData> {
+export async function update(userId: string, data: any): Promise<UserData> {
   return await userServiceClient.put(userId, data) as UserData;
 }
 
@@ -88,9 +99,15 @@ export async function update(userId:string, data:any): Promise<UserData> {
  * @permission Delete your own user object
  * @permission DELETE_USER | scope:global | Delete any user
  * @params {string} userId of the targeted user (required)
+ * @throws 404:
+ * {
+    "code": 16,
+    "name": "RESOURCE_UNKNOWN_EXCEPTION",
+    "message": "Requested resource is unknown"
+  }
  * @returns {boolean} success
  */
-export async function remove(userId:string): Promise<boolean> {
+export async function remove(userId: string): Promise<boolean> {
   const response: recordsAffectedResponse = await userServiceClient.delete(userId);
   return response.recordsAffected === 1;
 }
@@ -102,9 +119,21 @@ export async function remove(userId:string): Promise<boolean> {
  * @permission UPDATE_USER_EMAIL | scope:global | Update any user
  * @params {string} userId of the targeted user (required)
  * @params {string} email
+ * @throws 400:
+ * {
+    "code": 203,
+    "name": "EMAIL_USED_EXCEPTION",
+    "message": "This email address is already in use"
+  }
+ * @throws 404:
+ * {
+    "code": 16,
+    "name": "RESOURCE_UNKNOWN_EXCEPTION",
+    "message": "Requested resource is unknown"
+  }
  * @returns {UserData} UserData
  */
-export async function updateEmail(userId:string, email:string): Promise<UserData> {
+export async function updateEmail(userId: string, email: string): Promise<UserData> {
   return await userServiceClient.put(`${userId}/email`, { email }) as UserData;
 }
 
@@ -113,9 +142,15 @@ export async function updateEmail(userId:string, email:string): Promise<UserData
  * @permission ADD_PATIENT | scope:global | Add any patient enlistment
  * @params {string} userId of the targeted user (required)
  * @params {string} groupId of the targeted group (required)
+ * @throws 400:
+ * {
+    "code": 203,
+    "name": "RESOURCE_ALREADY_EXISTS_EXCEPTION",
+    "message": "This resource already exists"
+  }
  * @returns {boolean} success
  */
-export async function addPatientEnlistment(userId:string, groupId:string): Promise<boolean> {
+export async function addPatientEnlistment(userId: string, groupId: string): Promise<boolean> {
   const response: recordsAffectedResponse = await userServiceClient.post(`${userId}/patient_enlistments`, { groupId });
   return response.recordsAffected === 1;
 }
@@ -127,9 +162,15 @@ export async function addPatientEnlistment(userId:string, groupId:string): Promi
  * @permission REMOVE_PATIENT | scope:global | Remove any patient enlistment
  * @params {string} userId of the targeted user (required)
  * @params {string} groupId of the targeted group (required)
+ * @throws 404:
+ * {
+    "code": 16,
+    "name": "RESOURCE_UNKNOWN_EXCEPTION",
+    "message": "Requested resource is unknown"
+  }
  * @returns {boolean} success
  */
-export async function deletePatientEnlistment(userId:string, groupId:string): Promise<boolean> {
+export async function deletePatientEnlistment(userId: string, groupId: string): Promise<boolean> {
   const response: recordsAffectedResponse = await userServiceClient.delete(`${userId}/patient_enlistments/${groupId}`);
   return response.recordsAffected === 1;
 }
@@ -139,8 +180,13 @@ export async function deletePatientEnlistment(userId:string, groupId:string): Pr
  * @permission Everyone can use this endpoint
  * @params {RegisterUserData} registerData Data necessary to register (required)
  * @returns {UserData} UserData
+ * @throws 400: {
+    "code": 203,
+    "name": "EMAIL_USED_EXCEPTION",
+    "message": "This email address is already in use"
+  }
  */
-export async function register(data:RegisterUserData): Promise<UserData> {
+export async function register(data: RegisterUserData): Promise<UserData> {
   return await userServiceClient.post('register', data) as UserData;
 }
 
@@ -149,10 +195,15 @@ export async function register(data:RegisterUserData): Promise<UserData> {
  * @permission Everyone can use this endpoint
  * @params {string} Old password (required)
  * @params {string} New password (required)
+ * @throws 400: {
+    "code": 208,
+    "name": "PASSWORD_EXCEPTION",
+    "message": "The provided password is not correct"
+  }
  * @returns {boolean} success
  */
-export async function updatePassword(oldPassword:string, newPassword:string):Promise<boolean> {
-  const result:resultResponse = await userServiceClient.put('password', { oldPassword, newPassword });
+export async function updatePassword(oldPassword: string, newPassword: string): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.put('password', { oldPassword, newPassword });
   return result.status === Results.Success;
 }
 
@@ -161,9 +212,29 @@ export async function updatePassword(oldPassword:string, newPassword:string):Pro
  * @permission Everyone can use this endpoint
  * @params {string} email (required)
  * @params {string} password (required)
+ * @throws 401: {
+    "code": 106,
+    "name": "AUTHENTICATION_EXCEPTION",
+    "message": "This password email combination is unknown"
+  }
+ * @throws 401: {
+    "code": 211,
+    "name": "LOGIN_TIMEOUT_EXCEPTION",
+    "message": "Login attempt too fast"
+  }
+ * @throws 401: {
+    "code": 212,
+    "name": "LOGIN_FREEZE_EXCEPTION",
+    "message": "Login timeout in progress, too many failed login attempts"
+  }
+ * @throws 401: {
+    "code": 213,
+    "name": "TOO_MANY_FAILED_ATTEMPTS_EXCEPTION",
+    "message": "Account is locked due to too many failed login attempts"
+  }
  * @returns {UserData} UserData
  */
-export async function authenticate(email:string, password:string): Promise<UserData> {
+export async function authenticate(email: string, password: string): Promise<UserData> {
   return await userServiceClient.post('authenticate', { email, password }) as UserData;
 }
 
@@ -172,9 +243,19 @@ export async function authenticate(email:string, password:string): Promise<UserD
  * @permission Everyone can use this endpoint
  * @params {string} email (required)
  * @returns {boolean} success
+ * @throws 400: {
+    "code": 202,
+    "name": "EMAIL_UNKNOWN_EXCEPTION",
+    "message": "This email is not known"
+  }
+ * @throws 400: {
+    "code": 206,
+    "name": "ALREADY_ACTIVATED_EXCEPTION",
+    "message": "This user is already activated"
+  }
  */
-export async function requestActivationMail(email:string):Promise<boolean> {
-  const result:resultResponse = await userServiceClient.get(`activation?email=${email}`);
+export async function requestActivationMail(email: string): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.get(`activation?email=${email}`);
   return result.status === Results.Success;
 }
 
@@ -182,10 +263,15 @@ export async function requestActivationMail(email:string):Promise<boolean> {
  * Complete an email activation
  * @permission Everyone can use this endpoint
  * @params {string} hash (required)
+ * @throws 400: {
+    "code": 205,
+    "name": "ACTIVATION_UNKNOWN_EXCEPTION",
+    "message": "This activation does not exist"
+  }
  * @returns {boolean} success
  */
-export async function completeActivationMail(hash:string):Promise<boolean> {
-  const result:resultResponse = await userServiceClient.post('activation', { hash });
+export async function completeActivationMail(hash: string): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.post('activation', { hash });
   return result.status === Results.Success;
 }
 
@@ -193,10 +279,20 @@ export async function completeActivationMail(hash:string):Promise<boolean> {
  * Request a password reset.
  * @permission Everyone can use this endpoint
  * @params {string} email (required)
+ * @throws 400: {
+    "code": 202,
+    "name": "EMAIL_UNKNOWN_EXCEPTION",
+    "message": "This email is not known"
+  }
+ * @throws 400: {
+    "code": 204,
+    "name": "NOT_ACTIVATED_EXCEPTION",
+    "message": "This account needs to be activated before this action can be performed"
+  }
  * @returns {boolean} success
  */
-export async function requestPasswordReset(email:string):Promise<boolean> {
-  const result:resultResponse = await userServiceClient.get(`forgot_password?email=${email}`);
+export async function requestPasswordReset(email: string): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.get(`forgot_password?email=${email}`);
   return result.status === Results.Success;
 }
 
@@ -205,10 +301,20 @@ export async function requestPasswordReset(email:string):Promise<boolean> {
  * @permission Everyone can use this endpoint
  * @params {string} email (required)
  * @params {string} new password (required)
+ * @throws 400: {
+    "code": 204,
+    "name": "NOT_ACTIVATED_EXCEPTION",
+    "message": "This account needs to be activated before this action can be performed"
+  }
+ * @throws 400: {
+    "code": 207,
+    "name": "NEW_PASSWORD_HASH_UNKNOWN_EXCEPTION",
+    "message": "This new password hash does not exist"
+  }
  * @returns {boolean} success
  */
-export async function completePasswordReset(hash:string, newPassword:string):Promise<boolean> {
-  const result:resultResponse = await userServiceClient.post('forgot_password', { hash, newPassword });
+export async function completePasswordReset(hash: string, newPassword: string): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.post('forgot_password', { hash, newPassword });
   return result.status === Results.Success;
 }
 
@@ -216,10 +322,30 @@ export async function completePasswordReset(hash:string, newPassword:string):Pro
  * Confirm the password for the user making the request
  * @permission Everyone can use this endpoint
  * @params {string} password (required)
+ * @throws 401: {
+    "code": 106,
+    "name": "AUTHENTICATION_EXCEPTION",
+    "message": "This password email combination is unknown"
+  }
+ * @throws 401: {
+    "code": 211,
+    "name": "LOGIN_TIMEOUT_EXCEPTION",
+    "message": "Login attempt too fast"
+  }
+ * @throws 401: {
+    "code": 212,
+    "name": "LOGIN_FREEZE_EXCEPTION",
+    "message": "Login timeout in progress, too many failed login attempts"
+  }
+ * @throws 401: {
+    "code": 213,
+    "name": "TOO_MANY_FAILED_ATTEMPTS_EXCEPTION",
+    "message": "Account is locked due to too many failed login attempts"
+  }
  * @returns {boolean} success
  */
-export async function confirmPassword(password:string):Promise<boolean> {
-  const result:resultResponse = await userServiceClient.post('confirm_password', { password });
+export async function confirmPassword(password: string): Promise<boolean> {
+  const result: resultResponse = await userServiceClient.post('confirm_password', { password });
   return result.status === Results.Success;
 }
 
@@ -229,7 +355,7 @@ export async function confirmPassword(password:string):Promise<boolean> {
  * @params {string} email (required)
  * @returns {boolean} success
  */
-export async function emailAvailable(email:string):Promise<boolean> {
+export async function emailAvailable(email: string): Promise<boolean> {
   const result = await userServiceClient.get(`email_available?email=${email}`);
   return result.emailAvailable;
 }
