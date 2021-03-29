216504326f4c20d57a0a9c90a9c1e7a19650e773
Jens Verbeken <jens.verbeken@gmail.com>
2021-03-29 17:17:11 +0200

    update validate

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ import { createHttpClient, addAuth1, addAuth2 } from './http';
 function validateConfig({ apiHost, ...config }: Config): Config {
   return {
     ...config,
-    apiHost: apiHost.endsWith('/') ? apiHost : `${apiHost}/`,
+    apiHost: apiHost.endsWith('/')
+      ? apiHost.substr(0, apiHost.length - 1)
+      : apiHost,
   };
 }
 
