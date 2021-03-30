b0742da8d3499c21f03ed8f032032df7ea15e54b
Jens Verbeken <jens.verbeken@gmail.com>
2021-03-30 16:14:26 +0200

    fix eslint / prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
-const { off } = require("process")
-
 module.exports = {
-  extends: ['@qompium/eslint-config-q-node'],
+  extends: [
+    '@qompium/eslint-config-q-node',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   parserOptions: {
     project: './tsconfig.eslint.json'
-  },
+  },  
+  plugins: [
+    '@typescript-eslint',
+  ],
+  parser: "@typescript-eslint/parser",
   rules: {
     "max-classes-per-file": 0,
     "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.test.ts", "**/*.spec.ts"]}]
