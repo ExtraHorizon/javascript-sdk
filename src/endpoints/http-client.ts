31307fe831881a39ffc54a96a5e15df48e882418
Gal Dubitski <galmail@gmail.com>
2021-03-30 10:57:10 +0100

    added basic integration tests

--- src/endpoints/http-client.ts
+++ src/endpoints/http-client.ts
@@ -7,9 +7,7 @@ interface HttpClient {
 export default ({ BASE_PATH, transformRequestData }: HttpClient) => {
   console.log('BASE_PATH`', BASE_PATH);
   return {
-    get: (axios: AxiosInstance, path: string, config?: AxiosRequestConfig) =>
-      axios.get(`${BASE_PATH}${path}`, config),
-    put: (axios: AxiosInstance, path: string, data) =>
-      axios.put(`${BASE_PATH}${path}`, transformRequestData(data)),
+    get: (axios: AxiosInstance, path: string, config?: AxiosRequestConfig) => axios.get(`${BASE_PATH}${path}`, config),
+    put: (axios: AxiosInstance, path: string, data) => axios.put(`${BASE_PATH}${path}`, transformRequestData(data)),
   };
 };
