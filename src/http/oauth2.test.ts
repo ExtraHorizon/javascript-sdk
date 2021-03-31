ce3f5f2979560bb04396a52b30c2a7730783beea
Jens Verbeken <jens.verbeken@gmail.com>
2021-03-31 15:59:15 +0200

    add test

--- src/http/oauth2.test.ts
+++ src/http/oauth2.test.ts
@@ -52,7 +52,7 @@ describe('http client', () => {
     }
   });
 
-  it('Make call with authorization but first reply with expired token', async () => {
+  it('Make call with authorization but first reply with expired token, but then valid refresh', async () => {
     const mockToken = 'expired access token';
     nock(mockParams.apiHost)
       .post('/auth/v2/oauth2/token')
@@ -73,5 +73,57 @@ describe('http client', () => {
     const result = await httpWithAuth.get('test');
 
     expect(result.data).toBeDefined();
+  });
+
+  it('Make call with authorization but first reply with expired token, but then fail refresh', async () => {
+    expect.assertions(2);
+    const mockToken = 'expired access token';
+    nock(mockParams.apiHost)
+      .post('/auth/v2/oauth2/token')
+      .reply(200, { access_token: mockToken });
+
+    nock(mockParams.apiHost).get('/test').reply(400, {
+      code: 118,
+      error: 'invalid_grant',
+      description: 'the associated authorization is expired',
+    });
+
+    nock(mockParams.apiHost).post('/auth/v2/oauth2/token').reply(400, {
+      error: 'invalid_grant',
+      description: 'The refresh token is unknown',
+    });
+
+    nock(mockParams.apiHost).get('/test').reply(200, {});
+
+    try {
+      await httpWithAuth.get('test');
+    } catch (error) {
+      expect(error).toBeInstanceOf(ApiError);
+      expect(error.response.error).toBe('invalid_grant');
+    }
+  });
+
+  it('Make call with authorization but reply twice with unknown token', async () => {
+    const mockToken = 'unknown access token';
+    nock(mockParams.apiHost)
+      .post('/auth/v2/oauth2/token')
+      .reply(200, { access_token: mockToken });
+
+    nock(mockParams.apiHost).get('/test').reply(400, {
+      code: 117,
+      name: 'ACCESS_TOKEN_UNKNOWN_EXCEPTION',
+      message: 'The access token is unknown',
+    });
+
+    nock(mockParams.apiHost).post('/auth/v2/oauth2/token').reply(400, {
+      error: 'invalid_grant',
+      description: 'this password email combination is unknown',
+    });
+
+    try {
+      await httpWithAuth.get('test');
+    } catch (error) {
+      expect(error).toBeInstanceOf(ApiError);
+    }
   });
 });
