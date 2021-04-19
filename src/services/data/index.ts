dd6d524307487610c9ad80a3bd7390118bb93d87
Gal Dubitski <galmail@gmail.com>
2021-04-19 15:25:12 +0100

    some cleanup and small changes based on feedback

--- src/services/data/index.ts
+++ src/services/data/index.ts
@@ -1,21 +1,16 @@
-import { decamelizeKeys } from 'humps';
 import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
 import infrastructureService from './infrastructureService';
 import { DATA_BASE } from '../../constants';
 
-export type UsersService = ReturnType<typeof infrastructureService>;
+export type InfrastructureService = ReturnType<typeof infrastructureService>;
 
-export default (
-  http: HttpInstance
-  // httpWithAuth: HttpInstance
-): UsersService => {
-  const userClient = httpClient({
+export default (http: HttpInstance): InfrastructureService => {
+  const client = httpClient({
     basePath: DATA_BASE,
-    transformRequestData: decamelizeKeys,
   });
 
-  const infrastructureMethods = infrastructureService(userClient, http);
+  const infrastructureMethods = infrastructureService(client, http);
 
   return {
     ...infrastructureMethods,
--- src/services/data/infrastructureService.ts
+++ src/services/data/infrastructureService.ts
@@ -2,14 +2,14 @@ import type { HttpInstance } from '../../types';
 import { ResultResponse } from '../models/Responses';
 import { Results } from '../models/Results';
 
-export default (userClient, http: HttpInstance) => ({
+export default (client, http: HttpInstance) => ({
   /**
    * Perform a health check
    * @returns any Success
-   * @throws {500 Error}
+   * @throws {ServerError}
    */
   async health(): Promise<boolean> {
-    const result: ResultResponse = await userClient.get(http, '/health');
+    const result: ResultResponse = await client.get(http, '/health');
     return result.status === Results.Success;
   },
 });
