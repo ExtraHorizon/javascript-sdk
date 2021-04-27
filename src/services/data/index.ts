e2e3d9098d9c5151036226aad8dea4f2159579cf
Gal Dubitski <galmail@gmail.com>
2021-04-27 13:42:07 +0100

    Feature: data service indexes (#121)
    * created indexes methods
    
    * added unit tests

--- src/services/data/index.ts
+++ src/services/data/index.ts
@@ -2,12 +2,14 @@ import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
 import infrastructureService from './infrastructureService';
 import schemasService from './schemasService';
+import indexesService from './indexesService';
 import statusesService from './statusesService';
 import propertiesService from './propertiesService';
 import { DATA_BASE } from '../../constants';
 
 export type DataService = ReturnType<typeof infrastructureService> &
   ReturnType<typeof schemasService> &
+  ReturnType<typeof indexesService> &
   ReturnType<typeof statusesService> &
   ReturnType<typeof propertiesService>;
 
@@ -21,12 +23,14 @@ export default (
 
   const infrastructureMethods = infrastructureService(client, http);
   const schemasMethods = schemasService(client, httpWithAuth);
+  const indexesMethods = indexesService(client, httpWithAuth);
   const statusesMethods = statusesService(client, httpWithAuth);
   const propertiesMethods = propertiesService(client, httpWithAuth);
 
   return {
     ...infrastructureMethods,
     ...schemasMethods,
+    ...indexesMethods,
     ...statusesMethods,
     ...propertiesMethods,
   };
