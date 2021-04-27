c63fa271d0351b8ec3fa91809ea448b1897c3015
Gal Dubitski <galmail@gmail.com>
2021-04-27 13:38:47 +0100

    Feature: data service statuses (#120)
    * statuses service endpoints added
    
    * cleanup
    
    * added unit tests

--- src/services/data/index.ts
+++ src/services/data/index.ts
@@ -2,11 +2,13 @@ import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
 import infrastructureService from './infrastructureService';
 import schemasService from './schemasService';
+import statusesService from './statusesService';
 import propertiesService from './propertiesService';
 import { DATA_BASE } from '../../constants';
 
 export type DataService = ReturnType<typeof infrastructureService> &
   ReturnType<typeof schemasService> &
+  ReturnType<typeof statusesService> &
   ReturnType<typeof propertiesService>;
 
 export default (
@@ -19,11 +21,13 @@ export default (
 
   const infrastructureMethods = infrastructureService(client, http);
   const schemasMethods = schemasService(client, httpWithAuth);
+  const statusesMethods = statusesService(client, httpWithAuth);
   const propertiesMethods = propertiesService(client, httpWithAuth);
 
   return {
     ...infrastructureMethods,
     ...schemasMethods,
+    ...statusesMethods,
     ...propertiesMethods,
   };
 };
