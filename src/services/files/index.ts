a68d010835efc0748cf1a45f75a70da1e66b72c8
Jens Verbeken <jens.verbeken@gmail.com>
2021-04-28 12:45:12 +0200

    renames

--- src/services/files/index.ts
+++ src/services/files/index.ts
@@ -1,20 +1,19 @@
 import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
-import filesFilesService from './filesService';
-import tokensService from './tokensService';
+import files from './files';
+import tokens from './tokens';
 
 import { FILES_BASE } from '../../constants';
 
-export type FilesService = ReturnType<typeof filesFilesService> &
-  ReturnType<typeof tokensService>;
+export type FilesService = ReturnType<typeof files> & ReturnType<typeof tokens>;
 
 export const filesService = (httpWithAuth: HttpInstance): FilesService => {
   const client = httpClient({
     basePath: FILES_BASE,
   });
 
-  const filesMethods = filesFilesService(client, httpWithAuth);
-  const tokensMethods = tokensService(client, httpWithAuth);
+  const filesMethods = files(client, httpWithAuth);
+  const tokensMethods = tokens(client, httpWithAuth);
 
   return {
     ...filesMethods,
--- src/services/tasks/index.ts
+++ src/services/tasks/index.ts
@@ -1,16 +1,16 @@
 import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
-import tasksTasksService from './tasksService';
+import tasks from './tasks';
 import { TASKS_BASE } from '../../constants';
 
-export type TasksService = ReturnType<typeof tasksTasksService>;
+export type TasksService = ReturnType<typeof tasks>;
 
 export const tasksService = (httpWithAuth: HttpInstance): TasksService => {
   const client = httpClient({
     basePath: TASKS_BASE,
   });
 
-  const tasksMethods = tasksTasksService(client, httpWithAuth);
+  const tasksMethods = tasks(client, httpWithAuth);
 
   return {
     ...tasksMethods,
