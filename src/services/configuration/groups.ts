45b16f94f5a6a56f2eb1965bc4ca7788f248ca8a
Gal Dubitski <galmail@gmail.com>
2021-05-19 17:00:21 +0100

    saving progress

---
+++ src/services/configuration/groups.ts
@@ -0,0 +1,94 @@
+import type { HttpInstance } from '../../types';
+import type { ObjectId, AffectedRecords } from '../types';
+// import type {  } from './types';
+import type { RQLString } from '../../rql';
+
+// FIXME replace the missing types
+type Entity = unknown;
+type GroupConfiguration = unknown;
+type Timestamps = unknown;
+
+export default (client, httpAuth: HttpInstance) => ({
+  /**
+   * View a group configuration
+   * Permission | Scope | Effect
+   * - | - | -
+   * `VIEW_PATIENT_CONFIGURATIONS` | `staff enlistment` | View the group its patient configuration
+   * `VIEW_STAFF_CONFIGURATIONS` | `staff enlistment` | View the group its staff configuration
+   * `VIEW_CONFIGURATIONS` | `staff enlistment` | View the group its full configuration
+   * `VIEW_CONFIGURATIONS` | `global` | View any group its full configuration
+   *
+   * @param groupId The id of the targeted group
+   * @returns any Success
+   * @throws {ApiError}
+   */
+  async getGroupsConfig(
+    groupId: ObjectId
+  ): Promise<Entity & GroupConfiguration & Timestamps> {
+    return (await client.get(httpAuth, `/groups/${groupId}`)).data;
+  },
+
+  /**
+   * Update a group configuration.
+   * Permission | Scope | Effect
+   * - | - | -
+   * `UPDATE_PATIENT_CONFIGURATIONS` | `staff enlistment` | Update the group its patient configuration
+   * `UPDATE_STAFF_CONFIGURATIONS` | `staff enlistment` | Update the group its staff configuration
+   * `UPDATE_CONFIGURATIONS` | `staff enlistment` | Update the group its full configuration
+   * `UPDATE_CONFIGURATIONS` | `global` | Update any group its full configuration
+   *
+   * @param groupId The id of the targeted group
+   * @param rql Add filters to the requested list.
+   * @param requestBody
+   * @returns any Operation successful
+   * @throws {ApiError}
+   */
+  async updateGroupsConfig(
+    groupId: ObjectId,
+    requestBody: GroupConfiguration,
+    options?: {
+      rql?: RQLString;
+    }
+  ): Promise<AffectedRecords> {
+    return (
+      await client.put(
+        httpAuth,
+        `/groups/${groupId}${options?.rql || ''}`,
+        requestBody
+      )
+    ).data;
+  },
+
+  /**
+   * Delete fields from a group configuration.
+   * Permission | Scope | Effect
+   * - | - | -
+   * `UPDATE_PATIENT_CONFIGURATIONS` | `staff enlistment` | Update the group its patient configuration
+   * `UPDATE_STAFF_CONFIGURATIONS` | `staff enlistment` | Update the group its staff configuration
+   * `UPDATE_CONFIGURATIONS` | `staff enlistment` | Update the group its full configuration
+   * `UPDATE_CONFIGURATIONS` | `global` | Update any group its full configuration
+   *
+   * @param groupId The id of the targeted group
+   * @param rql Add filters to the requested list.
+   * @param requestBody
+   * @returns any Operation successful
+   * @throws {ApiError}
+   */
+  async removeFieldsFromGroupsConfig(
+    groupId: ObjectId,
+    requestBody: {
+      fields: Array<string>;
+    },
+    options?: {
+      rql?: RQLString;
+    }
+  ): Promise<AffectedRecords> {
+    return (
+      await client.post(
+        httpAuth,
+        `/groups/${groupId}/deleteFields${options?.rql || ''}`,
+        requestBody
+      )
+    ).data;
+  },
+});
