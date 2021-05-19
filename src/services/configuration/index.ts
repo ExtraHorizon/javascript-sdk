620fc8071dccee73cd02924c21d6929502feb9d4
Gal Dubitski <galmail@gmail.com>
2021-05-19 23:46:44 +0100

    added groups service

--- src/services/configuration/index.ts
+++ src/services/configuration/index.ts
@@ -1,11 +1,13 @@
 import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
 import general from './general';
+import groups from './groups';
 import { CONFIGURATION_BASE } from '../../constants';
 
 export * from './types';
 
-export type ConfigurationService = ReturnType<typeof general>;
+export type ConfigurationService = ReturnType<typeof general> &
+  ReturnType<typeof groups>;
 
 export const configurationService = (
   httpWithAuth: HttpInstance
@@ -15,8 +17,10 @@ export const configurationService = (
   });
 
   const generalMethods = general(client, httpWithAuth);
+  const groupsMethods = groups(client, httpWithAuth);
 
   return {
     ...generalMethods,
+    ...groupsMethods,
   };
 };
