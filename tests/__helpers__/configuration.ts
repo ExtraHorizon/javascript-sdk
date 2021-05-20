7d73a06c257eb52d06a74faa3521ebf124d7eed1
Gal Dubitski <galmail@gmail.com>
2021-05-20 16:59:55 +0100

    added unit tests for general service

---
+++ tests/__helpers__/configuration.ts
@@ -0,0 +1,24 @@
+export const generalConfig = {
+  data: {
+    epicFeatureEnabled: true,
+  },
+  userConfiguration: {
+    epicFeatureEnabled: true,
+  },
+  groupConfiguration: {
+    epicFeatureEnabled: true,
+  },
+  staffConfiguration: {
+    epicFeatureEnabled: true,
+  },
+  patientConfiguration: {
+    epicFeatureEnabled: true,
+  },
+};
+
+export const generalConfigResponse = {
+  ...generalConfig,
+  id: 'abcdef123456789abcdef123',
+  updateTimestamp: '2018-04-24T11:57:44.525Z',
+  creationTimestamp: '2018-04-24T11:57:44.525Z',
+};
