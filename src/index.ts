aaa7e9feeb759719ddc98b1eb2c46e45995eedbd
Jens Verbeken <jens.verbeken@gmail.com>
2021-05-26 11:58:49 +0200

    Add service descriptions

--- src/index.ts
+++ src/index.ts
@@ -82,14 +82,50 @@ type Authenticate<
 
 export interface Client<T extends ClientParams> {
   rawAxios: AxiosInstance;
+  /**
+   * The template service manages templates used to build emails. It can be used to retrieve, create, update or delete templates as well as resolving them.
+   * @see https://developers.extrahorizon.io/services/templates-service/1.0.13/
+   */
   template: ReturnType<typeof templateService>;
+  /**
+   * Provides mail functionality for other services.
+   * @see https://developers.extrahorizon.io/services/mail-service/1.0.8-dev/
+   */
   mail: ReturnType<typeof mailService>;
+  /**
+   * A flexible data storage for structured data. Additionally, the service enables you to configure a state machine for instances of the structured data. You can couple actions that need to be triggered by the state machine, when/as the entities (instance of structured data) change their state. Thanks to these actions you can define automation rules (see later for more in depth description). These actions also make it possible to interact with other services.
+   * @see https://developers.extrahorizon.io/services/data-service/1.0.9/
+   */
   data: ReturnType<typeof dataService>;
+  /**
+   * A service that handles file storage, metadata & file retrieval based on tokens.
+   * @see https://developers.extrahorizon.io/services/files-service/1.0.1-dev/
+   */
   files: ReturnType<typeof filesService>;
+  /**
+   * Start functions on demand, directly or at a future moment.
+   * @see https://developers.extrahorizon.io/services/tasks-service/1.0.4/
+   */
   tasks: ReturnType<typeof tasksService>;
+  /**
+   * Provides storage for custom configuration objects. On different levels (general, groups, users, links between groups and users).
+   * @see https://developers.extrahorizon.io/services/configurations-service/2.0.2-dev/
+   */
   configuration: ReturnType<typeof configurationService>;
+  /**
+   * Configure actions that need to be invoked when a specific event is/was triggered.
+   * @see https://developers.extrahorizon.io/services/dispatchers-service/1.0.3-dev/
+   */
   dispatchers: ReturnType<typeof dispatchersService>;
+  /**
+   * The user service stands in for managing users themselves, as well as roles related to users and groups of users.
+   * @see https://developers.extrahorizon.io/services/users-service/1.1.7/
+   */
   users: ReturnType<typeof usersService>;
+  /**
+   * Provides authentication functionality. The Authentication service supports both OAuth 1.0a and OAuth 2.0 standards.
+   * @see https://developers.extrahorizon.io/services/auth-service/2.0.4-dev/
+   */
   auth: ReturnType<typeof authService> & {
     /**
      *  Confirm MFA method with token, methodId and code
