fc1777ab4673af43e538634a5ebedb59c3bb87ad
Gal Dubitski <galmail@gmail.com>
2021-05-26 12:41:28 +0100

    added test report coverage

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,20 @@ module.exports = {
     '/tests/e2e/',
     '/build/',
   ],
-  reporters: ['default', 'jest-junit'],
+  coverage: true,
+  // reporters: ['default', 'jest-junit'],
+  reporters: [
+    'default',
+    [
+      'jest-stare',
+      {
+        resultDir: 'test-report',
+        reportTitle: 'Test report generated with jest-stare!',
+        additionalResultsProcessors: ['jest-junit'],
+        coverageLink: '../coverage/lcov-report/index.html',
+        jestStareConfigJson: 'jest-stare.json',
+        jestGlobalConfigJson: 'globalStuff.json',
+      },
+    ],
+  ],
 };
