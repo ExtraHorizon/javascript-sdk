5a624544e7bbf6bd13911a140c72d044261fc25a
Gal Dubitski <galmail@gmail.com>
2021-06-02 19:47:11 +0100

    health service docs

--- src/services/payments/health.ts
+++ src/services/payments/health.ts
@@ -4,8 +4,7 @@ import { ResultResponse, Results } from '../types';
 export default (client, httpAuth: HttpInstance) => ({
   /**
    * Check if the service is available
-   * @returns any Success
-   * @throws ApiError
+   * @returns true if service is up and running
    */
   async health(): Promise<boolean> {
     const result: ResultResponse = await client.get(httpAuth, '/health');
