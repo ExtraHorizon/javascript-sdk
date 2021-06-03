22dbc9ad42864121126afdd1a7d00435f93addf1
Jens Verbeken <jens.verbeken@gmail.com>
2021-06-03 08:51:13 +0200

    add getMockSdk

--- src/mock.ts
+++ src/mock.ts
@@ -4,6 +4,18 @@ import { ClientParams } from './types';
 
 const { raw: _raw, ...sdk } = createClient({ host: '' } as any);
 
+/**
+ * Returns a mocked version of the SDK. Requires a mocking function like `jest.fn`
+ * @param fn
+ * @returns {Client<ClientParams>} mockSdk
+ * @example
+ * describe("mock SDK", () => {
+ *   const sdk = getMockSdk(jest.fn);
+ *   it("should be valid mock", async () => {
+ *     expect(sdk.data).toBeDefined();
+ *   });
+ * });
+ */
 export const getMockSdk = (fn): Client<ClientParams> => ({
   ...recursiveMap(value => (typeof value === 'function' ? fn : value))(sdk),
   raw: [
