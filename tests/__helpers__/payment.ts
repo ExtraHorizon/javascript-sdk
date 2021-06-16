51b0cbba174c4f9c167ee9f08be92d188b61c92d
Jens Verbeken <jens.verbeken@gmail.com>
2021-06-16 17:17:42 +0200

    prevent gzip

--- tests/__helpers__/payment.ts
+++ tests/__helpers__/payment.ts
@@ -227,16 +227,6 @@ export const appleNotification: AppleNotification = {
         web_order_line_item_id: 'string',
       },
     ],
-    pending_renewal_info: [
-      {
-        auto_renew_product_id: 'string',
-        auto_renew_status: 'string',
-        expiration_intent: 'string',
-        is_in_billing_retry_period: 'string',
-        original_transaction_id: 'string',
-        product_id: 'string',
-      },
-    ],
     status: 0,
   },
   bid: 'string',
