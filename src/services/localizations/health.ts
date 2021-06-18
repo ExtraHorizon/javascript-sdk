65ce00fd8ef120a69d5705751bb5a797cc2861c1
Gal Dubitski <galmail@gmail.com>
2021-06-18 09:40:06 +0100

    added health service

---
+++ src/services/localizations/health.ts
@@ -0,0 +1,14 @@
+import type { HttpInstance } from '../../types';
+import { Results, ResultResponse } from '../types';
+
+export default (client, http: HttpInstance) => ({
+  /**
+   * Perform a health check
+   * @permission Everyone can use this endpoint
+   * @returns {boolean} success
+   */
+  async health(): Promise<boolean> {
+    const result: ResultResponse = await client.get(http, '/health');
+    return result.status === Results.Success;
+  },
+});
--- src/services/localizations/index.ts
+++ src/services/localizations/index.ts
@@ -1,9 +1,11 @@
 import type { HttpInstance } from '../../types';
 import httpClient from '../http-client';
 import localizations from './localizations';
+import health from './health';
 import { LOCALIZATIONS_BASE } from '../../constants';
 
-export type LocalizationsService = ReturnType<typeof localizations>;
+export type LocalizationsService = ReturnType<typeof localizations> &
+  ReturnType<typeof health>;
 
 export const localizationsService = (
   httpWithAuth: HttpInstance
@@ -13,6 +15,7 @@ export const localizationsService = (
   });
 
   return {
+    ...health(client, httpWithAuth),
     ...localizations(client, httpWithAuth),
   };
 };
