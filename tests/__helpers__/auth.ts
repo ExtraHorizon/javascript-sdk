1f10ac4927c1f47fe27d97a3e2f0c94edc52289f
Gal Dubitski <galmail@gmail.com>
2021-07-08 16:35:01 +0100

    more refactoring

--- tests/__helpers__/auth.ts
+++ tests/__helpers__/auth.ts
@@ -23,31 +23,12 @@ export const newApplicationVersion = {
   creationTimestamp: '2019-04-15T11:23:19.670Z',
 };
 
-export const authorizationList = {
-  page: {
-    total: 1,
-    offset: 0,
-    limit: 20,
-  },
-  data: [
-    {
-      id: '507f191e810c19729de860ea',
-      userId: '507f191e810c19729de860ea',
-      clientId: '507f191e810c19729de860ea',
-      authorizationCode: 'string',
-      state: 'string',
-      updateTimestamp: '2021-04-06T11:08:29.616Z',
-      creationTimestamp: '2021-04-06T11:08:29.616Z',
-    },
-  ],
-};
-
-export const newAuthorization = {
+export const authorizationData = {
   id: '507f191e810c19729de860ea',
   userId: '507f191e810c19729de860ea',
   clientId: '507f191e810c19729de860ea',
   authorizationCode: 'string',
   state: 'string',
-  updateTimestamp: '2019-04-15T11:23:19.670Z',
-  creationTimestamp: '2019-04-15T11:23:19.670Z',
+  updateTimestamp: new Date('2021-04-06T11:08:29.616Z'),
+  creationTimestamp: new Date('2021-04-06T11:08:29.616Z'),
 };
