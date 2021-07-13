fc50e15a314be46c58a80a443a1d7e1005c3d8e4
Jens Verbeken <jens.verbeken@gmail.com>
2021-07-13 16:04:12 +0200

    configurations

--- src/services/configurations/staff.ts
+++ src/services/configurations/staff.ts
@@ -9,19 +9,6 @@ export default (
   client,
   httpAuth: HttpInstance
 ): ConfigurationsStaffService => ({
-  /**
-   * Update a staff configuration for a group of a user.
-   * Permission | Scope | Effect
-   * - | - | -
-   * `UPDATE_STAFF_CONFIGURATIONS` | `staff enlistment` | For staff of the group, update the staff enlistment configuration of the group
-   * `UPDATE_CONFIGURATIONS` | `staff enlistment` | For staff of the group, update the staff enlistment configuration of the group
-   * `UPDATE_CONFIGURATIONS` | `global` | Update any user its staff configuration
-   *
-   * @param groupId The id of the targeted group
-   * @param userId The id of the targeted user
-   * @param requestBody UserConfigurationInput
-   * @returns AffectedRecords
-   */
   async update(
     groupId: ObjectId,
     userId: ObjectId,
@@ -36,19 +23,6 @@ export default (
     ).data;
   },
 
-  /**
-   * Delete fields from a staff configuration for a group of a user.
-   * Permission | Scope | Effect
-   * - | - | -
-   * `UPDATE_STAFF_CONFIGURATIONS` | `staff enlistment` | For staff of the group, update the staff enlistment configuration of the group
-   * `UPDATE_CONFIGURATIONS` | `staff enlistment` | For staff of the group, update the staff enlistment configuration of the group
-   * `UPDATE_CONFIGURATIONS` | `global` | Update any user its staff configuration
-   *
-   * @param groupId The id of the targeted group
-   * @param userId The id of the targeted user
-   * @param requestBody the list of fields to remove
-   * @returns AffectedRecords
-   */
   async removeFields(
     groupId: ObjectId,
     userId: ObjectId,
