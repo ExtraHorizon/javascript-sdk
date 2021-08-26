0091ada1c096758cb6c433cae78abaa3ac5a0e7e
Jens Verbeken <jens.verbeken@gmail.com>
2021-08-26 16:23:01 +0200

    add levels

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -7,8 +7,13 @@ export async function findAllGeneric<T>(
   find: (
     options: OptionsWithRql
   ) => PagedResult<T> | PromiseLike<PagedResult<T>>,
-  options: OptionsWithRql
+  options: OptionsWithRql,
+  level = 1
 ): Promise<T[]> {
+  if (level === 1 && options?.rql && options.rql.includes('limit(')) {
+    throw new Error('Do not pass in limit operator with findAll');
+  }
+
   // return async options => {
   // Extra check is needed because this function is call recursively with updated RQL
   // But on the first run, we need to set the limit to the max to optimize
@@ -28,11 +33,15 @@ export async function findAllGeneric<T>(
   return result.page.total > result.page.offset + result.page.limit
     ? [
         ...result.data,
-        ...(await findAllGeneric(find, {
-          rql: rqlBuilder(options?.rql)
-            .limit(result.page.limit, result.page.offset + result.page.limit)
-            .build(),
-        })),
+        ...(await findAllGeneric(
+          find,
+          {
+            rql: rqlBuilder(options?.rql)
+              .limit(result.page.limit, result.page.offset + result.page.limit)
+              .build(),
+          },
+          level + 1
+        )),
       ]
     : result.data;
 }
