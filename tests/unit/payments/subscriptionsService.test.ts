21aabcde65f48f259250d26236bbefa76ca2c1f7
Jens Verbeken <jens.verbeken@gmail.com>
2021-09-15 16:37:03 +0200

    more tests

--- tests/unit/payments/subscriptionsService.test.ts
+++ tests/unit/payments/subscriptionsService.test.ts
@@ -44,6 +44,58 @@ describe('Subscriptions Service', () => {
     expect(res.data.length).toBeGreaterThan(0);
   });
 
+  it('should request a list of all subscription entitlements', async () => {
+    nock(`${host}${PAYMENTS_BASE}`)
+      .get('/subscriptions/entitlements/?limit(50)')
+      .reply(200, {
+        page: {
+          total: 65,
+          offset: 0,
+          limit: 50,
+        },
+        data: Array(50).fill(subscriptionEntitlementData),
+      })
+      .get('/subscriptions/entitlements/?limit(50,50)')
+      .reply(200, {
+        page: {
+          total: 65,
+          offset: 50,
+          limit: 50,
+        },
+        data: Array(15).fill(subscriptionEntitlementData),
+      });
+    const res = await sdk.payments.subscriptions.entitlements.findAll();
+    expect(res.length).toBe(65);
+  });
+
+  it('should request a list of all subscription entitlements via iterator', async () => {
+    nock(`${host}${PAYMENTS_BASE}`)
+      .get('/subscriptions/entitlements/?limit(50)')
+      .reply(200, {
+        page: {
+          total: 55,
+          offset: 0,
+          limit: 50,
+        },
+        data: Array(50).fill(subscriptionEntitlementData),
+      })
+      .get('/subscriptions/entitlements/?limit(50,50)')
+      .reply(200, {
+        page: {
+          total: 55,
+          offset: 50,
+          limit: 50,
+        },
+        data: Array(5).fill(subscriptionEntitlementData),
+      });
+    const entitlements =
+      sdk.payments.subscriptions.entitlements.findAllIterator();
+
+    await entitlements.next();
+    const thirdPage = await entitlements.next();
+    expect(thirdPage.value.data.length).toBe(5);
+  });
+
   it('should get a list of subscription events', async () => {
     nock(`${host}${PAYMENTS_BASE}`)
       .get('/subscriptions/events/')
@@ -52,5 +104,56 @@ describe('Subscriptions Service', () => {
     const res = await sdk.payments.subscriptions.events.find();
 
     expect(res.data.length).toBeGreaterThan(0);
+  });
+
+  it('should request a list of all subscription events', async () => {
+    nock(`${host}${PAYMENTS_BASE}`)
+      .get('/subscriptions/events/?limit(50)')
+      .reply(200, {
+        page: {
+          total: 65,
+          offset: 0,
+          limit: 50,
+        },
+        data: Array(50).fill(subscriptionEventData),
+      })
+      .get('/subscriptions/events/?limit(50,50)')
+      .reply(200, {
+        page: {
+          total: 65,
+          offset: 50,
+          limit: 50,
+        },
+        data: Array(15).fill(subscriptionEventData),
+      });
+    const res = await sdk.payments.subscriptions.events.findAll();
+    expect(res.length).toBe(65);
+  });
+
+  it('should request a list of all subscription events via iterator', async () => {
+    nock(`${host}${PAYMENTS_BASE}`)
+      .get('/subscriptions/events/?limit(50)')
+      .reply(200, {
+        page: {
+          total: 55,
+          offset: 0,
+          limit: 50,
+        },
+        data: Array(50).fill(subscriptionEventData),
+      })
+      .get('/subscriptions/events/?limit(50,50)')
+      .reply(200, {
+        page: {
+          total: 55,
+          offset: 50,
+          limit: 50,
+        },
+        data: Array(5).fill(subscriptionEventData),
+      });
+    const events = sdk.payments.subscriptions.events.findAllIterator();
+
+    await events.next();
+    const thirdPage = await events.next();
+    expect(thirdPage.value.data.length).toBe(5);
   });
 });
