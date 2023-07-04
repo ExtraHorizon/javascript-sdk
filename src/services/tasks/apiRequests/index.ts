0985fb068cbc6572c937aaa1305a95eb8466781e
Chad Pelly <chad.pelly@extrahorizon.com>
2023-07-04 14:39:27 +0200

    SVCS-1703 Added API Requests interface

--- src/services/tasks/apiRequests/index.ts
+++ src/services/tasks/apiRequests/index.ts
@@ -1,11 +1,47 @@
 import { HttpInstance } from '../../../../http/types';
 import { HttpClient } from '../../../http-client';
-import { ApiRequestService } from './types';
+import { ApiRequest, ApiRequestService } from './types';
 import logs from './logs';
+import { OptionsWithRql } from '../../types';
+import {
+  addPagersFn,
+  findAllGeneric,
+  findAllIterator,
+  FindAllIterator,
+} from '../../helpers';
 
 export default (
   client: HttpClient,
   httpAuth: HttpInstance
-): ApiRequestService => ({
-  logs: logs(client, httpAuth),
-});
+): ApiRequestService => {
+  async function query(options: OptionsWithRql) {
+    const { data } = await client.get(
+      httpAuth,
+      `/apiRequests${options?.rql || ''}`,
+      options
+    );
+    return data;
+  }
+
+  return {
+    async find(options?: OptionsWithRql) {
+      const result = await query(options);
+      return addPagersFn<ApiRequest>(query, options, result);
+    },
+
+    async findAll(options?: OptionsWithRql): Promise<ApiRequest[]> {
+      return findAllGeneric<ApiRequest>(query, options);
+    },
+
+    findAllIterator(options?: OptionsWithRql): FindAllIterator<ApiRequest> {
+      return findAllIterator<ApiRequest>(query, options);
+    },
+
+    async findFirst(options?: OptionsWithRql): Promise<ApiRequest> {
+      const result = await query(options);
+      return result.data[0];
+    },
+
+    logs: logs(client, httpAuth),
+  };
+};
--- src/services/tasks/apiRequests/types.ts
+++ src/services/tasks/apiRequests/types.ts
@@ -1,7 +1,88 @@
 import { ApiRequestLogsService } from './logs/types';
+import { ObjectId, OptionsWithRql, PagedResultWithPager } from '../../types';
+import { FindAllIterator } from '../../helpers';
 
 export interface ApiRequestService {
+  /**
+   * ## Retrieve a paged list of API Requests
+   *
+   * **Global Permissions:**
+   * - `VIEW_API_FUNCTION_REQUESTS` - Allows a user to view API Requests
+   * @param options {@link OptionsWithRql} - Add filters to the requested list
+   * @returns A paged list of API Requests {@link PagedResultWithPager PagedResultWithPager<ApiRequest>}
+   */
+  find(options?: OptionsWithRql): Promise<PagedResultWithPager<ApiRequest>>;
+
+  /**
+   * ## Retrieve a list of all API Requests
+   *
+   * **Global Permissions:**
+   * - `VIEW_API_FUNCTION_REQUESTS` - Allows a user to view API Requests
+   * @param options {@link OptionsWithRql} - Add filters to the requested list
+   * @returns An array of API Requests {@link ApiRequest ApiRequest[]}
+   * @throws {@link Error} Do not pass in limit operator with findAll
+   */
+  findAll(options?: OptionsWithRql): Promise<ApiRequest[]>;
+
+  /**
+   * ## Retrieve a paged list of API Requests
+   *
+   * **Global Permissions:**
+   * - `VIEW_API_FUNCTION_REQUESTS` - Allows a user to view API Requests
+   * @param options {@link OptionsWithRql} - Add filters to the requested list
+   * @returns An iterator for the queried API Requests {@link FindAllIterator FindAllIterator<ApiRequest>}
+   */
+  findAllIterator(options?: OptionsWithRql): FindAllIterator<ApiRequest>;
+
+  /**
+   * ## Retrieve the first queried API Request
+   *
+   * **Global Permissions:**
+   * - `VIEW_API_FUNCTION_REQUESTS` - Allows a user to view API Requests
+   * @param options {@link OptionsWithRql} - Add filters to the requested list
+   * @returns The first element of the queried API Requests {@link ApiRequest}
+   */
+  findFirst(options?: OptionsWithRql): Promise<ApiRequest>;
   logs: ApiRequestLogsService;
+}
+
+export interface ApiRequest {
+  /** The Extra Horizon document id */
+  id: ObjectId;
+  /** The unique identifier amongst all Functions */
+  functionName: string;
+  /**  The HTTP method used to target the API function. */
+  method: ApiFunctionRequestMethod;
+  /** The path portion of the URL that comes after the Function name. */
+  path: string;
+  /** The Extra Horizon user id */
+  userId: ObjectId;
+  /** The Extra Horizon application id */
+  applicationId: ObjectId;
+  /** The status code of the API Function's response */
+  statusCode: number;
+  /** The time the API Function was executed */
+  timestamp: Date;
+  error: ApiRequestError;
+}
+
+export interface ApiRequestError {
+  /** The Extra Horizon error name */
+  name: string;
+  /** The Extra Horizon error message */
+  message: string;
+  /** The error type defines where the error occurred during the API Function lifecycle and can have one of the following values:
+   * - `invocation` - errors that occur before invocation of the API Function
+   * - `runtime` - errors that occur during the execution of the API Function
+   * - `response` - errors that occur during the response validation after the API Function has been executed
+   */
+  type: ApiRequestErrorType;
+}
+
+export enum ApiRequestErrorType {
+  INVOCATION = 'invocation',
+  RUNTIME = 'runtime',
+  RESPONSE = 'response',
 }
 
 export enum ApiFunctionRequestMethod {
