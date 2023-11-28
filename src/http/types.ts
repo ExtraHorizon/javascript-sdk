838f12737f42693b954dacc933eff81fe25bffb2
Chad Pelly <chad.pelly@extrahorizon.com>
2023-11-28 16:48:27 +0100

    SVCS-2622 Corrected comments

--- src/http/types.ts
+++ src/http/types.ts
@@ -28,10 +28,10 @@ export type HttpRequestConfig = AxiosRequestConfig & {
   /**
    * **Providing a key with an object value**:
    * - The provided key is not ignored for case conversion on a request and response
-   * - The child keys of the object are ignored for case and date conversion on a request and response
+   * - The child keys of the object are ignored for case conversion on a request and response
    *
    * **Providing a key with a non-object value**:
-   * - The provided key is ignored for case and date conversion on a request and response
+   * - The provided key is not ignored for case conversion on a request and response
    */
   customKeys?: string[];
   /**
@@ -39,10 +39,10 @@ export type HttpRequestConfig = AxiosRequestConfig & {
    *
    * **Providing a key with an object value**:
    * - The provided key is not ignored for case conversion on a request
-   * - The child keys of the object are ignored for case and date conversion on a request
+   * - The child keys of the object are ignored for case conversion on a request
    *
    * **Providing a key with a non-object value**:
-   * - The provided key is ignored for case and date conversion on a request
+   * - The provided key is not ignored for case conversion on a request
    */
   customRequestKeys?: string[];
   /**
@@ -53,7 +53,7 @@ export type HttpRequestConfig = AxiosRequestConfig & {
    * - The child keys of the object are ignored for case and date conversion on a response
    *
    * **Providing a key with a non-object value**:
-   * - The provided key is ignored for case and date conversion on a request
+   * - The provided key is not ignored for case conversion on a response
    */
   customResponseKeys?: string[];
 };
