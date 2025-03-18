7fa141334d857b70f2300cee44ec7c15158c5ac6
Chad Pelly <chad.pelly@extrahorizon.com>
2025-03-18 16:33:26 +0100

    EXH-302 Fixed incorrect snake casing

--- src/services/notificationsV2/index.ts
+++ src/services/notificationsV2/index.ts
@@ -1,5 +1,4 @@
 import { NOTIFICATIONS_V2_BASE } from '../../constants';
-import { decamelizeRequestData } from '../../http/interceptors';
 import { AuthHttpClient } from '../../types';
 import httpClient from '../http-client';
 import health from './health';
@@ -9,7 +8,6 @@ import { NotificationV2Service } from './types';
 export const notificationsV2Service = (httpWithAuth: AuthHttpClient): ReturnType<typeof health> & NotificationV2Service => {
   const client = httpClient({
     basePath: NOTIFICATIONS_V2_BASE,
-    transformRequestData: decamelizeRequestData,
   });
 
   return {
