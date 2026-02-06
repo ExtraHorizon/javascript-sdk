54545d35cbc456a3ee86f5895696cab5d61cc312
Aäron Trippaers <aaron.trippaers@extrahorizon.com>
2026-02-06 11:10:22 +0100

    Fix tests

--- src/services/templatesV2/index.ts
+++ src/services/templatesV2/index.ts
@@ -1,13 +1,13 @@
 import { TEMPLATES_V2_BASE } from '../../constants';
 import { decamelizeRequestData } from '../../http/interceptors';
-import type { HttpInstance } from '../../types';
+import { AuthHttpClient } from '../../types';
 import httpClient from '../http-client';
 import health from './health';
 import templatesV2 from './templatesV2';
 import { TemplatesV2Service } from './types';
 
 export const templatesV2Service = (
-  httpWithAuth: HttpInstance
+  httpWithAuth: AuthHttpClient
 ): ReturnType<typeof health> & TemplatesV2Service => {
   const client = httpClient({
     basePath: TEMPLATES_V2_BASE,
